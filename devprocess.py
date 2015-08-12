64d5eb540534aa8176a2fe8df8f1a89299e14b2c
Akif Rahim <akif.rahim@maxpoint.com>
2015-08-12 18:15:09 -0500

    Updated config, triage and blocker commands

--- devprocess.py
+++ devprocess.py
@@ -19,9 +19,12 @@ CONFIG_FILE_PATH = os.path.join(os.path.expanduser("~"), ".devconfig")
 
 #defaults if not provided
 JIRA_URL = "http://jira"
+
+#TODO - Read from setup.py
+KEYRING_SERVICE_NAME = "devprocess"
 #PROJECT_NAME = "Test EOS"
 #BOARD_NAME = "Test EOS"
-EPIC_NAME = "[Ongoing] Production Bugs"
+#EPIC_NAME = "[Ongoing] Production Bugs"
 TRIAGE_PRIORITY_BLOCKER_NAME = "Blocker"
 BUG_TYPE_NAME = "Bug"
 EPIC_TYPE_NAME = "Epic"
@@ -33,22 +36,34 @@ def is_configured():
         print "Unable to proceed. Configuration has not been performed. For help on confguration enter:\n" \
               "'devproc config --help'"
         return False
+    return True
+
+def get_jira_connection(url, user):
+
+    password = keyring.get_password(KEYRING_SERVICE_NAME, user)
+
+    try:
+        jira = JIRA(url, basic_auth=(user, password))
+    except Exception as e:
+        if e.status_code == 403:
+            print "Unable to connect to JIRA. Check the jira-url, user and password. Try again.\n"
+            raise
+
+        print "Make sure you are on VPN or can get to your JIRA server from this network"
+        raise
+
+    return jira
 
 def config(args):
     #store in config file
     #print args
+    #store password in keyring
+    keyring.set_password(KEYRING_SERVICE_NAME, args.user, args.password)
 
     #validate config settings
     print "Validating configuration parameters..."
-    try:
-        jira = JIRA(args.jira_url, basic_auth=(args.user, args.password))
-    except Exception as e:
-        if e.status_code == 403:
-            print "Unable to connect to JIRA. Check the jira-url, user and password. Try again."
-            return
 
-        print "Unable to connect to JIRA. Make sure you are on VPN or can get to your JIRA server from this network"
-        return
+    jira = get_jira_connection(args.jira_url, args.user)
 
     project = next((b for b in jira.projects() if b.name == args.projectname), None)
     if not project:
@@ -90,8 +105,13 @@ def config(args):
         print 'Could not find a priority named "Blocker". Ensure it exists and try again.\n'
         return
 
-    epic_issue = jira.search_issues("Project = '{0}' AND issueType = 'Epic' AND 'Epic Name' = '{1}'".format(
+    epic_issue = None
+
+    epic_issues = jira.search_issues("Project = '{0}' AND issueType = 'Epic' AND 'Epic Name' = '{1}'".format(
         args.projectname, args.epic_name))
+
+    if epic_issues:
+        epic_issue = epic_issues[0]
 
     if not epic_issue:
         epic_data = {
@@ -101,12 +121,10 @@ def config(args):
             "project": project.key
         }
 
-        #print epic_data
-
         print 'Could not find an Epic named "{0}". Creating it..."\n'.format(args.epic_name)
 
         try:
-            jira.create_issue(epic_data)
+            epic_issue = jira.create_issue(epic_data)
         except:
             print "Failure: Unable to create epic named {0}. Create the epic in project {1} and try again.".format(
                 args.epic_name, args.projectname)
@@ -115,67 +133,111 @@ def config(args):
         print "Created epic."
 
 
-    # epic = jira.search_issues('"Epic Name" = "{0}" AND project = "{1}"'.format(args.epic_name, args.projectname))
-    # if not epic:
-    #     print 'Unable to find epic named "{0}" in project "{1}". Creating it...'.format(args.epic_name, args.projectname)
-
     #save to file
     config = ConfigParser.RawConfigParser()
 
     config.add_section("Defaults")
     config.set("Defaults", "user", '"{0}"'.format(args.user))
+    config.set("Defaults", "jira_url", '"{0}"'.format(args.jira_url))
     config.set("Defaults", "project_name", '"{0}"'.format(args.projectname))
     config.set("Defaults", "board_name", '"{0}"'.format(args.boardname))
     config.set("Defaults", "epic_name", '"{0}"'.format(args.epic_name))
+    config.set("Defaults", "epic_key", '"{0}"'.format(epic_issue.key))
+
 
     config.add_section("Internal Field IDs")
-    config.set("Internal Field IDs", "Priority", '"{0}"'.format(priority_field_name))
-    config.set("Internal Field IDs", "Story Points", '"{0}"'.format(story_points_field_name))
-    config.set("Internal Field IDs", "Epic Links", '"{0}"'.format(epic_links_field_name))
-    config.set("Internal Field IDs", "Epic Name", '"{0}"'.format(epic_name_field_name))
-    config.set("Internal Field IDs", "Sprint", '"{0}"'.format(sprint_field_name))
+    config.set("Internal Field IDs", '"Priority"', '"{0}"'.format(priority_field_name))
+    config.set("Internal Field IDs", '"Story Points"', '"{0}"'.format(story_points_field_name))
+    config.set("Internal Field IDs", '"Epic Links"', '"{0}"'.format(epic_links_field_name))
+    config.set("Internal Field IDs", '"Epic Name"', '"{0}"'.format(epic_name_field_name))
+    config.set("Internal Field IDs", '"Sprint"', '"{0}"'.format(sprint_field_name))
+    config.set("Internal Field IDs", "priority_blocker_id", '"{0}"'.format(priority_blocker.id))
 
 
     #store password in keyring
-    keyring.set_password("devprocess", args.user, args.password)
+    #keyring.set_password(KEYRING_SERVICE_NAME, args.user, args.password)
 
     with open(CONFIG_FILE_PATH, "w") as f:
         config.write(f)
 
     print "Default Configuration set succesfully in {0}".format(CONFIG_FILE_PATH)
 
+def get_config():
 
+    """
+
+    :rtype : Dict
+    """
+    config = ConfigParser.SafeConfigParser()
+    config.read(CONFIG_FILE_PATH)
+
+    config_data = {}
+
+    for s in config.sections():
+        for i in config.items(s):
+            config_data[i[0].strip('"')] = i[1].strip('"')
+
+    #print config_data
+
+    return config_data
 
 def triage(args):
 
     if not is_configured():
         return
-    #print args
-    #print "IN TRIAGE"
-    #print "Issue Key: {0}".format(args.issuekey)
-    # TODO: Move these to config
-    # TODO: this should still check for assignee, issuekey and current sprint
 
     issue_key = args.issuekey
     story_points = args.storypoints
     assignee_name = args.assignee
+
+    config_data = get_config()
+
+    user = config_data["user"]
+    jira_url = config_data["jira_url"]
+    project_name = config_data["project_name"]
+    board_name = config_data["board_name"]
+    priority_blocker_id = config_data["priority_blocker_id"]
+
+    priority_field_name = config_data["priority"]
+    #TODO: "epic_name" can be confused with "epic name" but these are different values
+    epic_links_field_name = config_data["epic links"]
+    epic_key = config_data["epic_key"]
+    story_points_field_name = config_data["story points"]
+    sprint_field_name = config_data["sprint"]
+
+    jira = get_jira_connection(jira_url, user)
     
-    jira =  JIRA(JIRA_URL, basic_auth=(args.user, args.password))
+    #jira =  JIRA(JIRA_URL, basic_auth=(args.user, args.password))
     issue = jira.issue(issue_key)
 
-    #get the current Sprint - DO NOT move this to a config file as this will change often
-    #it should always be found on every invocation
-    current_sprint = None
+
     
     board_id = None
     for board in jira.boards():
-        if board.name == BOARD_NAME:
+        if board.name == board_name:
             board_id = board.id
             break
     
     if not board_id:
-        print 'Unable to find board "{0}" Will not be able to assign the issue to the active sprint'.format(BOARD_NAME)
-    
+        print 'Unable to proceed. Did not find board "{0}". Ensure that this board exists and run:\n' \
+              'devproc config --help'.format(board_name)
+        return
+
+
+    data = {}
+    #Issue with JIRA API that causes a 500 error if fields are already set
+    #Only update fields that are not set correctly
+    if getattr(issue.fields(), priority_field_name).id != str(priority_blocker_id):
+        data[priority_field_name] = { "id": str(priority_blocker_id) }
+
+    if getattr(issue.fields(), epic_links_field_name) != epic_key:
+        data[epic_links_field_name] = epic_key
+
+    if getattr(issue.fields(), story_points_field_name) != story_points:
+        data[story_points_field_name] = story_points
+
+    #get the current Sprint - DO NOT move this to a config file as this will change often
+    #record but proceed if active sprint does not exist
     sprint_id = None
     for sprint in jira.sprints(board_id, True):
         if sprint.state == "ACTIVE":
@@ -183,33 +245,23 @@ def triage(args):
             break
 
     if not sprint_id:
-        print "Could not find an active sprint. Issue will not be assigned to a sprint."
+        print "Could not find an active sprint. Proceeding with triage but the Issue will not be assigned to a sprint."
 
-    data = {}
-    #Issue with JIRA API that causes a 500 error if fields are already set
-    #Only update fields that are not set correctly
-    if getattr(issue.fields(), priority_field_name).id != str(priority_blocker.id):
-        data[priority_field_name] = { "id": str(priority_blocker.id) }
-
-    if getattr(issue.fields(), epic_field_name) != epic_issue[0].key:
-        data[epic_field_name] = epic_issue[0].key 
-
-    if getattr(issue.fields(), story_points_field_name) != story_points:
-        data[story_points_field_name] = story_points
-    
+    #if sprint exists and is already assigned
     is_sprint_assigned = False
-    sprints =  getattr(issue.fields(), sprint_field_name)
-    if sprints:
-        for sprint in sprints:
-            if sprint.split("id=")[-1].strip("]") == str(sprint_id):
-                is_sprint_assigned = True
-                break
-    
-    if not is_sprint_assigned:   
-    #if getattr(issue.fields(), sprint_field_name)[0].split("id=")[-1].strip("]") != str(sprint_id):
+    if sprint_id:
+        sprints =  getattr(issue.fields(), sprint_field_name)
+        if sprints:
+            for sprint in sprints:
+                if sprint.split("id=")[-1].strip("]") == str(sprint_id):
+                    is_sprint_assigned = True
+                    break
+
+    if sprint_id and (not is_sprint_assigned):
+        #if getattr(issue.fields(), sprint_field_name)[0].split("id=")[-1].strip("]") != str(sprint_id):
         data[sprint_field_name] = str(sprint_id)
-    
-    #TODO: Validate that assignee exists as a user in JIRA
+
+    #TODO: Validate that assignee exists as a user in JIRA otherwise fail it
     is_assigned = False
     if issue.fields().assignee:
         if issue.fields().assignee.name == assignee_name:
@@ -230,22 +282,33 @@ def blocker(args):
 
     if not is_configured():
         return
-    #print args
+
     summary = args.summary
 
-    jira =  JIRA(JIRA_URL, basic_auth=(args.user, args.password))
+    config_data = get_config()
 
-    project_key = next((p.key for p in jira.projects() if p.name == PROJECT_NAME), None)
+    user = config_data["user"]
+
+    jira_url = config_data["jira_url"]
+
+    project_name = config_data["project_name"]
+
+    #print jira_url, user, password
+
+    jira = get_jira_connection(jira_url, user)
+    #jira =  JIRA(jira_url, basic_auth=(user, password))
+
+    project_key = next((p.key for p in jira.projects() if p.name == project_name), None)
 
     if not project_key:
-        print "Unable to find project {0}. Failed to create a new issue."
+        print 'Unable to find project "{0}". Failed to create a new issue.'.format(project_name)
         return
 
     data = {
         "project": project_key,
         "issuetype": { "name": BUG_TYPE_NAME },
         "priority": { "name": TRIAGE_PRIORITY_BLOCKER_NAME },
-        "assignee": { "name": DEFAULT_ASSIGNEE_NAME },
+        #"assignee": { "name": DEFAULT_ASSIGNEE_NAME },
         "summary": summary
 
     }
